8dca25835479bab20192810547766e1ea52a07ed
TogrulAli <allahmanli@yahoo.com>
2022-07-07 19:18:52 +0200

    task3

---
+++ js_tasks/task3/app.js
@@ -0,0 +1,10 @@
+let num = prompt('Enter the number');
+if(num > 0) {
+    console.log('number is plus');
+}
+else if(num<0) {
+    console.log('number is minus');
+}
+else{
+    console.log('number is zero');
+}
